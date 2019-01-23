0d13b37cf94ae4b000f4bc6db98d65b2155da58e
Krishan <jambakrishan@gmail.com>
2019-01-23 11:19:54 -0500

    commit number 2

--- new project 1/Program.cs
+++ new project 1/Program.cs
@@ -10,6 +10,17 @@ namespace new_project_1
     {
         static void Main(string[] args)
         {
+            int sum = 0;
+            int i = 0;
+            for (; i < 5;)
+            {
+                i++;
+                sum += i;
+            }
+
+
+            Console.WriteLine("the sum of number is {0}", sum);
+            Console.ReadLine();
         }
     }
 }
